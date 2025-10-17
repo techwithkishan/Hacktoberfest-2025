e7eceb7e3af4cdd6da5c31b30b70a71be869ec86
Jay Kumar <jk316875@gmail.com>
2025-10-17 14:05:08 +0530

    candy dash

---
+++ Jay/candy dash/src/main/java/com/CandyDashApplication.java
@@ -0,0 +1,11 @@
+package com.example.candydash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyDashApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CandyDashApplication.class, args);
+    }
+}
